1a19ca8140a95b3fc62ed3e5919d5110f22da108
Manjinder <singh.mj95@gmail.com>
2020-04-14 15:39:56 +0530

    Updated interface of custom pool

--- common/db/customPool.go
+++ common/db/customPool.go
@@ -8,11 +8,15 @@ import (
 	"github.com/gocql/gocql"
 )
 
-// This pool implementation is really naive, there's a room for a lot of improvements
+// CustomPool : This pool implementation is really naive, there's a room for a lot of improvements
+// TODO: Locking consumes too much of cpu cycles, try finding some good way of doing it
+// perhaps look into the mutex implementation and use spin locks, if required
 type CustomPool struct {
-	cluster  *gocql.ClusterConfig
-	sessions []*gocql.Session
-	poolLock *sync.Mutex
+	cluster               *gocql.ClusterConfig
+	sessions              []*gocql.Session
+	poolLock              *sync.Mutex
+	connectionsAllocated  int // these are atomic values
+	connectionsToAllocate int // these are atomic values
 }
 
 // Init :
@@ -21,33 +25,55 @@ func (helper *CustomPool) Init(ip string, keyspace string) {
 	helper.cluster.Keyspace = keyspace
 	helper.cluster.Consistency = gocql.Quorum
 	helper.poolLock = &sync.Mutex{}
+	helper.connectionsAllocated = 0
+	helper.connectionsToAllocate = 0
+}
+
+// IncreasePoolSize :
+func (helper *CustomPool) IncreasePoolSize(newPoolSize int) error {
+	helper.poolLock.Lock()
+	defer helper.poolLock.Unlock()
+
+	if newPoolSize < helper.connectionsAllocated {
+		return errors.New("you cannot scale down the database pool")
+	}
+	helper.connectionsToAllocate++
+	return nil
 }
 
 // GetNewSession :
 func (helper *CustomPool) GetNewSession() (*gocql.Session, error) {
+	helper.poolLock.Lock()
+	defer helper.poolLock.Unlock()
 	return CreateSession(helper.cluster)
 }
 
 // GetSessionFromPool :
 func (helper *CustomPool) GetSessionFromPool() (*gocql.Session, error) {
-	if helper.poolLock == nil {
-		fmt.Println("The pool lock is an empty object")
-		return nil, errors.New("jasdjasnjd")
-	}
 	helper.poolLock.Lock()
 	defer helper.poolLock.Unlock()
 
+	// since this path is protected by the lock, we can increment the value
 	if len(helper.sessions) == 0 {
+		// already have created the required number of sessions, and they all have been exhausted now
+		// can't proceed, either call IncreasePoolSize with requested params or
+		// better retry in some time
+		if helper.connectionsAllocated >= helper.connectionsToAllocate {
+			return nil, errors.New("Pool doesn't have any session to return")
+		}
+
 		fmt.Println("Pool doesn't have any sessions in it. Going to create a new session in the pool.")
 		session, err := CreateSession(helper.cluster)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to created a new session. more info: %v", err)
 		}
 		helper.sessions = append(helper.sessions, session)
+		helper.connectionsAllocated++
 	}
 
 	session, sessions := helper.sessions[len(helper.sessions)-1], helper.sessions[:len(helper.sessions)-1]
 	helper.sessions = sessions
+	helper.connectionsAllocated--
 	return session, nil
 }
 
@@ -56,10 +82,14 @@ func (helper *CustomPool) ReturnSessionToPool(session *gocql.Session) {
 	helper.poolLock.Lock()
 	defer helper.poolLock.Unlock()
 	helper.sessions = append(helper.sessions, session)
+	helper.connectionsAllocated++
 }
 
 // Connect :
 func (helper *CustomPool) Connect(poolSize int) error {
+	helper.poolLock.Lock()
+	defer helper.poolLock.Unlock()
+	helper.connectionsToAllocate = poolSize
 	helper.sessions = make([]*gocql.Session, poolSize)
 	for i := 0; i < poolSize; i++ {
 		session, err := CreateSession(helper.cluster)
@@ -67,8 +97,8 @@ func (helper *CustomPool) Connect(poolSize int) error {
 			return err
 		}
 		helper.sessions = append(helper.sessions, session)
+		helper.connectionsAllocated--
 	}
-
 	return nil
 }
 
@@ -76,6 +106,8 @@ func (helper *CustomPool) Connect(poolSize int) error {
 // make sure that you return all of the sessions back to pool
 // before calling this func
 func (helper *CustomPool) Disconnect() {
+	helper.poolLock.Lock()
+	defer helper.poolLock.Unlock()
 	for _, session := range helper.sessions {
 		session.Close()
 	}
